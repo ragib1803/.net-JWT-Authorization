de861d434e50b9308f719f0d5c0f92fb2cca7f08
ragib1803 <ragibalam@live.com>
2024-04-25 16:41:08 +0600

    Middleware updated

--- jwt_token/Authorization/Middleware.cs
+++ jwt_token/Authorization/Middleware.cs
@@ -1,8 +1,32 @@
-﻿using System;
+﻿namespace WebApi.Authorization;
 
+using Microsoft.Extensions.Options;
+using WebApi.Helpers;
+using WebApi.Services;
 public class Middleware
 {
 	public Middleware()
 	{
+		private readonly RequestDelegate _next;
+    private readonly AppSettings _appSettings;
+
+    public JwtMiddleware(RequestDelegate next, IOptions<AppSettings> appSettings)
+    {
+        _next = next;
+        _appSettings = appSettings.Value;
+    }
+
+    public async Task Invoke(HttpContext context, IUserService userService, IJwtUtils jwtUtils)
+    {
+        var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+        var userId = jwtUtils.ValidateJwtToken(token);
+        if (userId != null)
+        {
+            // attach user to context on successful jwt validation
+            context.Items["User"] = userService.GetById(userId.Value);
+        }
+
+        await _next(context);
+    }
 	}
 }
