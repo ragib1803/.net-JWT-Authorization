a3f617258d366e3ca0d00af3674c3e0c1823df1f
Umaar_Abdullah <umaar71@gmail.com>
2024-04-25 16:09:55 +0600

    Added Custom Authorize Attribute

--- jwt_token/Models/Users.cs
+++ jwt_token/Models/Users.cs
@@ -1,8 +1,18 @@
-﻿namespace jwt_token.Models
+﻿using System.Text.Json.Serialization;
+
+namespace jwt_token.Models
 {
     public class Users
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
+
+        [JsonIgnore]
+        public string PasswordHash { get; set; }
+
+        //[JsonIgnore]
+        // public List<RefreshToken> RefreshTokens { get; set; }
     }
 }
